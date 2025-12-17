729078b0b9aa7e769ea8ea5f49aa50dee53d1101
grishmakhatra39 <grishmakhatra39@gmail.com>
2025-12-17 12:13:30 -0500

    second time

---
+++ app/app.py
@@ -0,0 +1,28 @@
+import sys
+import os
+
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(ROOT_DIR)
+
+import streamlit as st
+from pipeline.pipeline import AnimeRecommendationPipeline
+from dotenv import load_dotenv
+
+st.set_page_config(page_title="Anime Recommnder",layout="wide")
+
+load_dotenv()
+
+@st.cache_resource
+def init_pipeline():
+    return AnimeRecommendationPipeline()
+
+pipeline = init_pipeline()
+
+st.title("Anime Recommender System")
+
+query = st.text_input("Enter your anime prefernces eg. : light hearted anime with school settings")
+if query:
+    with st.spinner("Fetching recommendations for you....."):
+        response = pipeline.recommend(query)
+        st.markdown("### Recommendations")
+        st.write(response)
